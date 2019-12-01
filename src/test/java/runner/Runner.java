54d6171a8c3d2b10637b063c20407b4b80658268
Diana Elena <diana.apolinar@hotmail.com>
2019-12-01 12:43:28 -0600

    Add Runner

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -1,4 +1,15 @@
 package runner;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "html:target/cucumber-reports","json:target/cucumber.json"},
+        glue = {"com.consol.citrus.cucumber.step.designer"},
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+        tags = {"@General"}
+)
 public class Runner {
 }
