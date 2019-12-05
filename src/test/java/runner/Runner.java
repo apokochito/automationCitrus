156bfba1dd756150f33ad22590a5b046da667f16
Diana Elena <diana.apolinar@hotmail.com>
2019-12-04 23:28:05 -0600

    Fixing dependencies

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -1,14 +1,15 @@
 package runner;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
+@CucumberOptions(features = "src/test/resources/features",
         plugin = {"pretty", "html:target/cucumber-reports","json:target/cucumber.json"},
         glue = {"com.consol.citrus.cucumber.step.designer"},
-        snippets = CucumberOptions.SnippetType.CAMELCASE,
+        snippets = SnippetType.CAMELCASE,
         tags = {"@Test"}
 )
 public class Runner {
